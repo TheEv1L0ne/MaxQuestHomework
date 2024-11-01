03c330d57c6e918b5cd93db0278cae354bbd285f
Marko Leskovsek <marko.leskovsek.91@gmail.com>
2024-11-01 23:12:39 +0100

    Added simple gun that rotates towards mouse

---
+++ Assets/_Scripts/Gun.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private GameObject bullet;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtMouse();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnBullet();
+        }
+    }
+
+    private void LookAtMouse()
+    {
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mouseScreenPosition - (Vector2) transform.position).normalized;
+        transform.up = direction;
+    }
+
+    private void SpawnBullet()
+    {
+        GameObject bulletGo = Instantiate(bullet, bulletSpawnPoint.transform.position, Quaternion.identity);
+        
+    }
+}
