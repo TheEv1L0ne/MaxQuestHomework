77d17686736ec3ef10828150ea859d65a05cbe29
Marko Leskovsek <marko.leskovsek.91@gmail.com>
2024-11-02 13:00:46 +0100

    Fish will now move towards center

--- Assets/_Scripts/Fish.cs
+++ Assets/_Scripts/Fish.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
+    [SerializeField] private float speed = 4f;
+    private Vector3 _moveVector = Vector3.zero;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitStartDirection();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveFish();
+    }
+    
+    private void MoveFish()
+    {
+        transform.position += _moveVector * Time.deltaTime * speed;
+    }
+
+    private void InitStartDirection()
+    {
+        _moveVector = (Vector3.zero - transform.position).normalized;
     }
 }
