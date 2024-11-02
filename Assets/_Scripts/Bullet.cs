dddb0a6372a20905db1d56ef73e7ca900e92e80f
Marko Leskovsek <marko.leskovsek.91@gmail.com>
2024-11-02 10:58:15 +0100

    Bullets will now bounce of screen edges

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -7,20 +7,46 @@ public class Bullet : MonoBehaviour
 
     [SerializeField] private float speed = 4f;
     
+    private Vector3 _moveVector = Vector3.zero;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _moveVector = transform.up.normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveBullet();
+        CheckForBounce();
     }
 
     private void MoveBullet()
     {
-        transform.position += transform.up.normalized * Time.deltaTime * speed;
+        transform.position += _moveVector * Time.deltaTime * speed;
+    }
+
+    private void CheckForBounce()
+    {
+        float ortoSize = Camera.main.orthographicSize;
+        
+        float width = 16f / 9f * ortoSize;
+
+        var pos = this.transform.position;
+
+        if (pos.y > ortoSize ||  pos.y < -ortoSize)
+        {
+            var moveV = _moveVector;
+            moveV.y = -moveV.y;
+            _moveVector = moveV;
+        }
+        
+        if(pos.x < -width || pos.x > width)
+        {
+            var moveV = _moveVector;
+            moveV.x = -moveV.x;
+            _moveVector = moveV;
+        }
     }
 }
